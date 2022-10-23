f2a766f7a7826c01c99a018b5bb98be9c6d38b63
AlexDuncan2100 <alexduncan2100@gmail.com>
2022-10-23 07:32:08 +0000

    added dev-k8s-terraform  for kubernetes infrastructure2

--- infrastructure/dev-k8s-terraform/modules/IAM/variables.tf
+++ infrastructure/dev-k8s-terraform/modules/IAM/variables.tf
@@ -1,31 +1,31 @@
 variable "master-role-policy" {
-  default = "petclinic_policy_for_master_role"
+  default = "petclinic_policy_for_master_role-de"
 }
 
 variable "worker-role-policy" {
-  default = "petclinic_policy_for_worker_role"
+  default = "petclinic_policy_for_worker_role-de"
 }
 
 variable "master-role" {
-  default = "petclinic_role_master_k8s"
+  default = "petclinic_role_master_k8s-de"
 }
 
 variable "worker-role" {
-  default = "petclinic_role_worker_k8s"
+  default = "petclinic_role_worker_k8s-de"
 }
 
 variable "master-role-attachment" {
-  default = "petclinic_attachment_for_master"
+  default = "petclinic_attachment_for_master-de"
 }
 
 variable "worker-role-attachment" {
-  default = "petclinic_attachment_for_worker"
+  default = "petclinic_attachment_for_worker-de"
 }
 
 variable "profile_for_master" {
-  default = "petclinic_profile_for_master"
+  default = "petclinic_profile_for_master-de"
 }
 
 variable "profile_for_worker" {
-  default = "petclinic_profile_for_worker"
+  default = "petclinic_profile_for_worker-de"
 }
