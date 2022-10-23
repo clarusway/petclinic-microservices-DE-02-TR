545e359629ffa83e5f1b378ecb6b7f31d4a3ec67
AlexDuncan2100 <alexduncan2100@gmail.com>
2022-10-23 07:54:33 +0000

    added dev-k8s-terraform  for kubernetes infrastructure3

--- infrastructure/dev-k8s-terraform/main.tf
+++ infrastructure/dev-k8s-terraform/main.tf
@@ -7,15 +7,15 @@ module "iam" {
 }
 
 variable "sec-gr-mutual" {
-  default = "petclinic-k8s-mutual-sec-group"
+  default = "petclinic-k8s-mutual-sec-group-de"
 }
 
 variable "sec-gr-k8s-master" {
-  default = "petclinic-k8s-master-sec-group"
+  default = "petclinic-k8s-master-sec-group-de"
 }
 
 variable "sec-gr-k8s-worker" {
-  default = "petclinic-k8s-worker-sec-group"
+  default = "petclinic-k8s-worker-sec-group-de"
 }
 
 data "aws_vpc" "name" {
